ed0e5a8ec9f76814ea490764e2ca3a985101e9f0
Maxwell Hanson <m47hanson47@gmail.com>
2023-07-21 14:25:55 -0400

    comments

--- src/app/coffeereviews/[id]/page.js
+++ src/app/coffeereviews/[id]/page.js
@@ -27,13 +27,17 @@ export const getStaticProps = async (context) =>{
     const id = context.params.id;
     const response = await fetch('http://localhost:1337/api/coffee-reviews/' + id);
     let data =  response.json();
+
+    //experimental line below, seems to have no effect
+    // const reviewJSON = JSON.stringify(data);
+
     //unneccesarry line below? returns undefined for review either way
     data = data.data
 
     //should i be returning data.data instead??
 
     return {
-        props: {review: data}
+        props: {review: reviewJSON}
     }
 }
 
@@ -44,6 +48,7 @@ export const getStaticProps = async (context) =>{
 
 
 const Details = ({review}) =>{
+
     //currently prints undefined !
     console.log(review)
 
