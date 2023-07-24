3cea0b003dfa1f3fc84345aed1791403e0ba81e8
Maxwell Hanson <m47hanson47@gmail.com>
2023-07-24 09:24:08 -0400

    trying ot render obj

--- src/app/coffeereviews/[id]/page.js
+++ src/app/coffeereviews/[id]/page.js
@@ -1,64 +1,59 @@
 'use client';
+import React, { useEffect, useState } from 'react';
+
+import axios from 'axios';
 
 
-export const getStaticPaths = async () =>{
-    const response = await fetch('http://localhost:1337/api/coffee-reviews');
-    const data = response.json();
-
-    //not too sure about the code from here to the end of the function
-    //i know we are trying to collect all the review ids for paths
-    //just not sure if this is executed correctly
-    const paths = data.data.map(review=>{
-        return{
-            params: {id: review.id.toString()}
-        }
-    })
-
-    return {
-        paths: paths,
-        fallback: false
-    }
-}
+const Details = () => {
 
 
-
-export const getStaticProps = async (context) =>{
-
-    const id = context.params.id;
-    const response = await fetch('http://localhost:1337/api/coffee-reviews/' + id);
-    let data =  response.json();
-
-    //experimental line below, seems to have no effect
-    // const reviewJSON = JSON.stringify(data);
-
-    //unneccesarry line below? returns undefined for review either way
-    data = data.data
-
-    //should i be returning data.data instead??
-
-    return {
-        props: {review: reviewJSON}
-    }
-}
+    const [reviewObj, setData] = useState([]);
 
 
 
 
+    useEffect(() => {
+
+        // Function to fetch data
+
+        const fetchData = async () => {
+            try {
+                const response =  await axios.get('http://localhost:1337/api/coffee-reviews/' + "1");
+                setData(response.data);
+                console.log(response.data);
+
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
 
 
 
-const Details = ({review}) =>{
+        fetchData();
 
-    //currently prints undefined !
-    console.log(review)
+    }, []);
 
-    return(
-        <div>
+
+    //trying to stringify data before displaying it and then just rendering data in the component's return statement will break the program.
+    //instad we createt a new constant and render that. 
+     const review = JSON.stringify(reviewObj);
+    //  console.log(review);
+    //  console.log()
+
+    return (
+
+        <div className = 'singleReview'>
+            
             <h1>Details page: {review}</h1>
-        
+
+
         </div>
+
     );
+
 };
 
-export default Details;
 
+
+
+export default Details;
