e8f1e2e0dea564746f862d0e5f71a781fe555ab9
Maxwell Hanson <m47hanson47@gmail.com>
2023-08-09 17:13:26 -0400

    got a name post working

--- src/app/testpage1/page.js
+++ src/app/testpage1/page.js
@@ -2,20 +2,21 @@
 import { useLogGood } from '../../../components/LogGoodContext';
 import { useUserContext } from '../../../components/UserContext';
 import {useState} from 'react';
-import useStrapi from '../../../hooks/useStrapi';
+import axios from 'axios';
+
 
 export default function testPage() {
+  //this logGood and activeUser stuff was for me to test and track certain things related to user login/logout please ignore for now
   const { logGood } = useLogGood();
   const { activeUser } = useUserContext();
   const [name, setName] = useState('');
-  const [file, setFile] = useState(null);
-  const {fetchAPI} = useStrapi();
+  // const [file, setFile] = useState(null);
   const myToken = '0da196ac90a697a22e3129cfa649885b5ee066d9bfb436d9ed546a89dd77c536b0732e13307e158a5cb222dff08b918553976e8e56e12002709e55988277d32d6638ce7db4a82cac47cd3e6939b5d0ad670972461372794fe7313781376f80f74ec66174cd8e99e41bba828d52bb68ec2e69a7575abe9ddf99020a7f78a4809e'
 
 
   console.log('Value of logGood:', logGood + '. Value of active user: ' + activeUser);
 
-  const form = new FormData();
+  const formData = new FormData();
 
   const handleNameChange = (e) =>{
     setName(e.target.value);
@@ -25,28 +26,24 @@ export default function testPage() {
   //   setFile(e.target.files[0])
   // }
 
-  const postData={
-    "data": {
-      "Name": name
-    }
-  }
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = async (e) =>{
     e.preventDefault();
     console.log('name:' + name);
+    formData.append('Name', name);
 
-    const response = fetchAPI('/api/tests',
-    {
-      method:'POST',
-      data: postData, 
+
+    const response = await axios.post('http://localhost:1337/api/tests',{
+      "data": {
+        "Name": "Hello",
+        }
     },{
     headers:{
-    'Authorization': `Bearer${myToken}`
-    }})
-    
-    if (response.status===200){
-      console.log('success');
+    'Authorization': `Bearer ${myToken}`,
     }
+    })
+
+    console.log(response.status);
   }
 
     return (
