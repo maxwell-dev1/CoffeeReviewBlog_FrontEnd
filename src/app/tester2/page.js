0311ec7888e803b57d5d21213aaf8c1bc645ac15
Maxwell Hanson <m47hanson47@gmail.com>
2024-03-19 17:36:50 -0400

    added links to carousel on tester2

--- src/app/tester2/page.js
+++ src/app/tester2/page.js
@@ -5,15 +5,24 @@ import useFetch from '../../../hooks/useFetch';
 import { useState, useEffect } from 'react';
 
 export default function Tester2() {
+
     const { loading, error, data } = useFetch('http://localhost:1337/api/coffee-reviews/?populate=*');
     const [reviewImages, setReviewImages] = useState([]);
+    const [reviewLinks, setReviewLinks] = useState([])
 
     useEffect(() => {
         if (!loading && !error && data) {
             const images = data.data.map(review => `http://localhost:1337${review.attributes.ProductImage.data[0].attributes.formats.medium.url}`);
             setReviewImages(images);
+            
+            const links = data.data.map(review => `http://localhost:3000/coffeereviews/${review.id}`)
+            setReviewLinks(links);
+
+
         }
     }, [loading, error, data]);
+
+
 
     if (loading) {
         return <p>Loading...</p>;
@@ -23,6 +32,8 @@ export default function Tester2() {
         return <p>Error! Failed to fetch info from Strapi backend</p>;
     }
 
+
+
     return (
         <div className="carouselContainer">
             <Box sx={{ width: '80%', margin: '0 auto', height: '140%', '& .css-1m9128y': { marginTop: '30px' } }}>
@@ -30,11 +41,11 @@ export default function Tester2() {
                     {reviewImages.map((image, i) => (
                         <Paper key={i} sx={{ backgroundColor: 'black', marginTop: '20px', height: '60%' }}>
                             <Box sx={{ width: '100%', height: '640px', display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'hidden', margin: '0 auto' }}>
-                                <img
+                                <a href={reviewLinks[i]}><img
                                     src={image}
                                     alt={`Review Image ${i}`}
                                     style={{ maxWidth: '100%', maxHeight: '100%',borderRadius:10 }}
-                                />
+                                /></a>
                             </Box>
                         </Paper>
                     ))}
