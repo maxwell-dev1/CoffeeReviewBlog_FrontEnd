5fcdf7ba267ce1f8f97d804d2b62e08ac0ab59c6
maxwell-dev1 <maxwelldevhanson@gmail.com>
2025-11-13 20:46:40 -0500

    first, commented out some code on rootpage

--- src/app/page.js
+++ src/app/page.js
@@ -18,27 +18,27 @@ export default function rootpage(){
     const {jwt} = useJwtContext();
     console.log('logGood = ' + logGood + '.' + ' active user is: ' + activeUser)
 
-    useEffect(() => {
-            if (!loading && !error && data) {
+    // useEffect(() => {
+    //         if (!loading && !error && data) {
 
-                const images = data.data.map(review => `http://localhost:1337${review.attributes.ProductImage.data[0].attributes.formats.medium.url}`);
-                setReviewImages(images);  
-                const links = data.data.map(review => `http://localhost:3000/coffeereviews/${review.id}`)
-                links.slice(0,5)
-                setReviewLinks(links);
-            }
-        }, [loading, error, data]);
-    if(loading){
-        return <p>Loading...</p>
-    }
-    if(error){
-        return <p>Error!...Failed to fetch info from Strapi back end</p>
-    }
+    //             const images = data.data.map(review => `http://localhost:1337${review.attributes.ProductImage.data[0].attributes.formats.medium.url}`);
+    //             setReviewImages(images);  
+    //             const links = data.data.map(review => `http://localhost:3000/coffeereviews/${review.id}`)
+    //             links.slice(0,5)
+    //             setReviewLinks(links);
+    //         }
+    //     }, [loading, error, data]);
+    // if(loading){
+    //     return <p>Loading...</p>
+    // }
+    // if(error){
+    //     return <p>Error!...Failed to fetch info from Strapi back end</p>
+    // }
 
-    console.log(data);    console.log(jwt)
+    // console.log(data);    console.log(jwt)
 
 
-    data.data.reverse()    //reversing the data array allows me to display most recent entries first
+    // data.data.reverse()    //reversing the data array allows me to display most recent entries first
 
 
     
@@ -58,12 +58,14 @@ export default function rootpage(){
             {/* Div contains both recent posts carousel and featured articles  but not their text titles above them   */}
             <div className="homeCardCont">
                 <Paper className="homeTopCards"  sx={{backgroundColor:"rgba(0,0,0,0)", marginLeft:10, width:"45%",marginTop:"28px"}} >
-                    {data.data.slice(0,5).map(review => ( 
+                    {/* {data.data.slice(0,5).map(review => ( 
                         <Card elevation={8} sx={{         backgroundColor: "rgba(219, 193, 172,.20)",
                         backgroundImage: "linear-gradient(to right, rgba(219, 193, 172, .15), rgba(0,0,0,.15))",marginBottom:6, height:"12%", backdropFilter: "blur(10px)"}}>
-                            <Typography variant='h4' sx={{marginLeft:6, marginTop:3,color:"#dbc1ac" }}>{review.attributes.Title.split(' ').slice(0, 5).join(' ')}</Typography> {/*split and slice makes the title display only the first 5 words */}
-                        </Card>
-                    ))} {/* replace with data for learn cofee articles instead of reviews */}
+                            <Typography variant='h4' sx={{marginLeft:6, marginTop:3,color:"#dbc1ac" }}>{review.attributes.Title.split(' ').slice(0, 5).join(' ')}</Typography>  */}
+                            {/*split and slice makes the title display only the first 5 words */}
+                        {/* </Card> */}
+                    {/* ))}  */}
+                    {/* replace with data for learn cofee articles instead of reviews */}
                 </Paper>
 
 
@@ -71,7 +73,7 @@ export default function rootpage(){
 
                         <Carousel sx={{  borderRadius: '12px',  width: '100%'  }}>
                             
-                            {reviewImages.map((image, i) => (
+                            {/* {reviewImages.map((image, i) => (
                                 <Paper key={i} sx={{ backgroundColor: 'black', marginTop: '20px', height: '60%', borderRadius:"12px" }}>
                                     <Box sx={{ width: '100%', height: '640px', display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'hidden', margin: '0 auto' ,}}>
                                         <a href={reviewLinks[i]}>
@@ -80,10 +82,9 @@ export default function rootpage(){
                                     </Box>
 
                                 </Paper>
-                            ))}
+                            ))} */}
                         </Carousel>
                     </Box>
-                {/* </Paper> */}
 
                 
             </div>
